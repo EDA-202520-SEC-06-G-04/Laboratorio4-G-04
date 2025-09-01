cc10553f5920a1f3aea66501cfedbde2e2b428e8
mgabrielah <mg.herrera@uniandes.edu.co>
2025-09-01 09:39:12 -0500

    queue_array

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -1,31 +1,27 @@
-from DataStructures.List import single_linked_list as sl
+from DataStructures.List import array_list as arl
 
 def new_queue():
-    return sl.new_list()
+    return arl.new_list()
 
 def enqueue (my_queue, element):
-    return sl.add_last(my_queue, element)
+    return arl.add_last(my_queue, element)
 
 def dequeue (my_queue):
     
-    if my_queue["size"] == 0:
+    if arl.is_empty(my_queue):
         raise Exception("EmptyStructureError: queue is empty")
-    elemento = my_queue["first"]["info"]
-    my_queue["first"] = my_queue["first"]["next"]
-    
-    if my_queue["first"] is None:
-        my_queue["last"] = None    
+    elemento = my_queue["elements"].pop(0) 
     my_queue ["size"]-=1
     return elemento
 
 def peek(my_queue):
-    if sl.is_empty(my_queue):
+    if arl.is_empty(my_queue):
         raise Exception("EmptyStructureError: queue is empty")
     
-    return sl.first_element(my_queue)
+    return arl.first_element(my_queue)
 
 def is_empty(my_queue):
-    return my_queue["size"] == 0
+    return arl.is_empty(my_queue)
 
 def size(my_queue):
-    return sl.size(my_queue)
+    return arl.size(my_queue)
